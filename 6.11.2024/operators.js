29a58c3f0b334b8cbff8cefe22949ebd6a47d903
Ameena Durrani <125674092+ameenadurrani@users.noreply.github.com>
2024-11-06 08:59:22 -0800

    Add files via upload

---
+++ 6.11.2024/operators.js
@@ -0,0 +1,44 @@
+console.log("Arithmetic Operators in JS");
+let a = 23;
+let b = 3;
+console.log("a + b = ", a+b);
+console.log("a - b = ", a-b);
+console.log("a * b = ", a*b);
+console.log("a / b = ", a/b);
+console.log("a ** b = ", a**b);
+console.log("a % b = ", a%b);
+console.log("a++ = ", a++);
+console.log("++a = ", ++a);
+console.log("a-- = ", a--);
+console.log("--a = ", --a);
+console.log("a = ", a);
+
+console.log("Assignment Operators in JS");
+let c = 4;
+console.log("c is: ",c);
+c+=5; //c = c + 5
+console.log("c is now(c+=5): ",c);
+c-=5; //c = c - 5
+console.log("c is now(c-=5): ",c);
+c*=5; //c = c * 5
+console.log("c is now(c*=5): ",c);
+c/=5; //c = c / 5
+console.log("c is now(c/=5): ",c);
+c%=5; //c = c % 5
+console.log("c is now(c%=5): ",c);
+c**=5; //c = c ** 5
+console.log("c is now(c**=5): ",c);
+
+console.log("Comparison Operators in JS");
+let d = 4;
+let e = 7;
+console.log("d == e is ", d==e);    //if d = 6 and e ='6' it will give true but e===d will give false because they have different types
+console.log("d != e is ", d!=e);     //if d = 6 and e ='6' it will give false but e!==d will give false because they have different types
+console.log("d === e is ", d===e);    
+console.log("d !== e is ", d!==e);    
+
+
+//logical operators
+//&& and
+//|| or
+//! not
