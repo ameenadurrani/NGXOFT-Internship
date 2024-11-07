ecaf3674fffc9ee5f097b29480586e5327e10059
Ameena Durrani <125674092+ameenadurrani@users.noreply.github.com>
2024-11-07 04:38:10 -0800

    Day2

--- 6.11.2024/operators.js
+++ 6.11.2024/operators.js
@@ -39,6 +39,14 @@ console.log("d !== e is ", d!==e);
 
 
 //logical operators
-//&& and
-//|| or
-//! not
+//&& and (it will be true if both the conditions will be true)
+//|| or (it will be true if one of the conditions will be true)
+//! not (it will negate the condition)
+
+//taking input from user in terminal (npm install prompt-sync)
+const ps = require("prompt-sync");
+const prompt = ps();
+let name = prompt("Enter your name: ");
+console.log(`Hello, ${name}`);
+let age = prompt("Enter your age: ");
+console.log(`You are ${age} years old.`);
